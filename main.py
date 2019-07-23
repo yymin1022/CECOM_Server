6f590f9d1d22e6dcd58bc4f793dbe8d11165448f
yymin1022 <yymin1022@gmail.com>
2019-07-23 15:37:33 +0900

    Add : File Download

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
-
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+#파일 목록에서 다운로드 함수 호출시 해당 파일의 경로 지정
+filename = "TODO.txt"
+
+@app.route('/download_file')
+def download_file():
+    file_name = filename
+    return send_file(file_name,
+                     mimetype='unknown',
+                     attachment_filename='filename',
+                     as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
